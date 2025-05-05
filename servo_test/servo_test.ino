8080ac011da13c7dd7390d9d8c4e6f02cfd283cc
ippinex <73573996+ippinex@users.noreply.github.com>
2025-05-05 14:06:18 +0900

    servo test

---
+++ servo_test/servo_test.ino
@@ -0,0 +1,16 @@
+#include <ESP32Servo.h>
+
+Servo myservo;
+
+void setup() {
+  myservo.attach(15); 
+  myservo.write(0);
+  delay(1000);
+  myservo.write(180);
+  delay(1000);
+  myservo.write(90); 
+}
+
+void loop() {
+  
+}
